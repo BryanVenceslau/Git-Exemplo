7155cd4d4af6ab92c32ae5917948837a66d67876
Bryan Venceslau <bryanvenceslauoficial@gmail.com>
2024-06-24 08:57:12 -0300

    Análise

--- src/gitexemplo/GitExemplo.java
+++ src/gitexemplo/GitExemplo.java
@@ -6,5 +6,6 @@ package gitexemplo;
 public class GitExemplo {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Pan-Americano");
     }
 }
